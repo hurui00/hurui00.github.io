cd3567407c4c7510023f4a18127975c7cb06f741
hurui <huruiaiwl@163.com>
2016-06-19 22:18:25 +0800

    第二版

---
+++ js/addMouseWheel.js
@@ -0,0 +1,28 @@
+//版权 北京智能社©, 保留所有权利
+
+function addMouseWheel(obj,fn){
+		
+	//1添加事件
+	if(window.navigator.userAgent.toLowerCase().indexOf("firefox") != -1){
+		//ff
+		obj.addEventListener("DOMMouseScroll",fnWheel,false);	
+	} else {
+		//ie chrome
+		obj.onmousewheel = fnWheel;
+	}	
+	
+	function fnWheel(ev){
+		var oEvent = ev || event;
+		var bDown = true;//下 为正
+		
+		if(oEvent.wheelDelta){
+			bDown = oEvent.wheelDelta > 0 ? false : true;
+		} else {
+			bDown = oEvent.detail > 0 ? true : false;	
+		}
+		
+		typeof fn == "function" && fn(bDown);
+		oEvent.preventDefault && oEvent.preventDefault();
+		return false;
+	} 
+}
